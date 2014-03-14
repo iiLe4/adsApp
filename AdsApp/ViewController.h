a961d190cf830a7863251d6a5c3614bc6e0cc95a
iiLe4 <ilepadilla4@gmail.com>
2014-03-13 19:42:28 -0600

    Initial Commit

---
+++ AdsApp/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  AdsApp
+//
+//  Created by Ileana Padilla on 13/03/14.
+//  Copyright (c) 2014 Ileana Padilla. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
